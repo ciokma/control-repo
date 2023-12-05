a44d930d182de795c239c57f7b3ca08c3e14e33c
mauricio <maurez89@yahoo.es>
2023-12-04 23:39:25 -0600

    fixing error scripts

--- site/profile/manifests/example.pp
+++ site/profile/manifests/example.pp
@@ -13,4 +13,19 @@ class profile::example {
     group  => 'ubuntu',    # Grupo de la carpeta
     }
 
+    # Define la ruta del archivo que deseas crear
+    $archivo_a_crear = '/var/tmp/creada-desde-puppet/archivo.txt'
+
+    # Define el contenido del archivo
+    $contenido_del_archivo = 'Soy creado desde puppet via r10k'
+
+    # Crea el archivo con el contenido utilizando el recurso file
+    file { $archivo_a_crear:
+    ensure  => file,            # Asegura que es un archivo
+    content => $contenido_del_archivo,  # Establece el contenido del archivo
+    mode    => '0644',          # Permisos para el archivo
+    owner   => 'ubuntu',       # Propietario del archivo
+    group   => 'ubuntu',         # Grupo del archivo
+    }
+
 }
