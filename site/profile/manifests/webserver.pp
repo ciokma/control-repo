162c8d61f4275a26be9a34e43a4d4469fa075e87
Mauricio Perez <Mauricio.Perez@davita.com>
2023-12-05 12:49:40 -0600

    agregando modulo

--- site/profile/manifests/webserver.pp
+++ site/profile/manifests/webserver.pp
@@ -1,7 +1,12 @@
 class profile::webserver
 {
     
-    class { 'stdlib': }
-    class { 'httpd': }
+    #class { 'stdlib': }
+    #class { 'httpd': }
+    service { 'apache2':
+    ensure => 'running',
+    enable => true,
+    require => Class['apache'],
+    }
 
 }
