8c7e075e37bfa3726486d0ab774cd37c1c1d2e7c
mauricio <maurez89@yahoo.es>
2023-12-04 23:37:05 -0600

    agregando roles

--- site/profile/manifests/example.pp
+++ site/profile/manifests/example.pp
@@ -1,3 +1,16 @@
 class profile::example {
 
+    # MiArchivoPuppet.pp
+
+    # Define la ruta de la carpeta que deseas crear
+    $carpeta_a_crear = '/var/tmp/creada-desde-puppet'
+
+    # Crea la carpeta utilizando el recurso file
+    file { $carpeta_a_crear:
+    ensure => directory,  # Asegura que es un directorio
+    mode   => '0755',     # Permisos para la carpeta
+    owner  => 'ubuntu',  # Propietario de la carpeta
+    group  => 'ubuntu',    # Grupo de la carpeta
+    }
+
 }
--- site/role/manifests/example.pp
+++ site/role/manifests/example.pp
@@ -1,16 +1,4 @@
 class role::example {
-
-    # MiArchivoPuppet.pp
-
-    # Define la ruta de la carpeta que deseas crear
-    $carpeta_a_crear = '/var/tmp/creada-desde-puppet'
-
-    # Crea la carpeta utilizando el recurso file
-    file { $carpeta_a_crear:
-    ensure => directory,  # Asegura que es un directorio
-    mode   => '0755',     # Permisos para la carpeta
-    owner  => 'ubuntu',  # Propietario de la carpeta
-    group  => 'grupo',    # Grupo de la carpeta
-    }
+  include profile::example  
 
 }
